53556688fb0154fe05a6ed55d60e4d8772aa7f72
coorujaah <thalia_amalia_da_silva@hotmail.com>
2025-08-08 23:07:31 -0400

    Add .next/static/chunks/src_app_produtos_page_tsx_a993527f._.js 1754708851865

---
+++ .next/static/chunks/src_app_produtos_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_79561e24._.js",
+  "static/chunks/src_fe5a2087._.js"
+],
+    source: "dynamic"
+});
